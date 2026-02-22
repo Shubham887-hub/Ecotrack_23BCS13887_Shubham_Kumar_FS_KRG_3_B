4329e1f44c66801209d564e25b0f13fdaa7f709c
Shubham887-hub <shubhamkumartiw2223@gmail.com>
2026-02-22 23:17:42 +0530

    Delete Exp-04 directory

--- Exp-04/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import logsReducer from "./logSlice";
-
-
-export const store = configureStore({
-    reducer:{
-        logs:logsReducer,
-    }
-})
